4ad6f8a3f6f1087fc3813991c72466ab41804846
Adel Cabrera <adel.cabrera.mesina@gmail.com>
2020-02-08 23:24:34 -0300

    fix

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/darkonnen/Documents/Projects/django_crud_todo/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
